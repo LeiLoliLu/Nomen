14127176a00e21c58426d59ac5e502723cce4247
Leire Gómez <smr14.2021jesuitas@gmail.com>
2022-03-31 12:59:22 +0200

    Subida Nomen V2

--- nomen.py
+++ nomen.py
@@ -1,9 +1,12 @@
+import time
 nomen="Nomen"
 
 def usernombre():
     user = input()
+    time.sleep(1.2)
     print(f"¿Quieres que me refiera a ti como {user}? (Y/N)")
     ans = input()
+    time.sleep(1.2)
     if ans=="Y":
         print(f"Encantado de conocerte, {user}")
     else:
@@ -11,6 +14,7 @@ def usernombre():
         usernombre()
 
 def presentacion():
+    time.sleep(1)
     print(f"¡Hola! Soy {nomen}. ¿Como te llamas?")
     usernombre()
 
